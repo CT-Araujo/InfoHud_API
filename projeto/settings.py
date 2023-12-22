99dc3e04df17c38c3d10e5187b7a3b0814382e42
Cleiton Araujo <cleitonaraujo1311@gmail.com>
2023-12-22 17:15:56 -0300

    Render Config

--- projeto/settings.py
+++ projeto/settings.py
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-68)8f8-_^spslr7bq3_lw*fv#^!g$+b!n6@c^867k^ms=+$=s('
+SECRET_KEY = os.environ.get('SECRET_KEY', default=SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    
 # Application definition
 
 INSTALLED_APPS = [
