f50559261a4abbb0f87352a7223cfed3594a274c
Cleiton Araujo <cleitonaraujo1311@gmail.com>
2024-01-01 21:46:12 -0300

    Nova migrate

--- app/models.py
+++ app/models.py
@@ -13,10 +13,10 @@ class Usuarios(models.Model):
     
 class Postagens(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key= True, editable=True, unique= True)
-    titulo = models.CharField(max_length=50, blank= False, null= False)
-    subtitulo = models.CharField(max_length=40, blank= False, null= False)
+    titulo = models.CharField(max_length=150, blank= False, null= False)
+    subtitulo = models.CharField(max_length=140, blank= False, null= False)
     categoria = models.CharField(max_length=50, blank= False, null= True)
-    miniurl = models.CharField(max_length=150, blank= True, null=True)
-    conteudo = models.CharField(max_length=500, blank=False, null=True)
+    miniurl = models.CharField(max_length=400, blank= True, null=True)
+    conteudo = models.CharField(max_length=4000, blank=False, null=True)
     user_nickname = models.CharField(max_length=50, blank=False, editable=True)    
     
